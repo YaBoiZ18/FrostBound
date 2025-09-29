5f463daf182df12ef8b101f2fe60689da11e035e
YaBoiZ18 <113143428+YaBoiZ18@users.noreply.github.com>
2025-09-29 00:15:49 -0500

    Examine system

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    Outline outline;
+    public string message;
+
+    public UnityEvent onInteraction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        DisableOutLine();
+    }
+
+    public void Interact() 
+    {
+        onInteraction.Invoke();
+    }
+
+    public void DisableOutLine()
+    {
+        outline.enabled = false;
+    }
+
+    public void EnableOutLine()
+    {
+        outline.enabled = true;
+    }
+
+}
